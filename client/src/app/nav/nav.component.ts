de707fa8c8c6354001e7e3e396f83e83162f8eb5
Dcode1213 <dilipahir1213@gmail.com>
2023-07-06 02:23:16 -0700

    End Of Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Route, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -14,7 +16,8 @@ model : any =  {};
 // loggedIn = false;
 // currentUser$ : Observable<User | null> = of(null);           // removed and directly used
 
-  constructor(public accountService: AccountService) { }   //make public
+  constructor(public accountService: AccountService,private router: Router,
+    private toastr: ToastrService) { }   
 
   ngOnInit(): void {
    // this.getCurrentUser();
@@ -30,20 +33,21 @@ model : any =  {};
 
   login(){
     this.accountService.login(this.model).subscribe({
-        next: response => {
-        console.log(response);
+        next: _ => this.router.navigateByUrl('/members'),      //_ means in there is no any arguments ,used insted of ()
+
+        error: error => this.toastr.error(error.error)                              //console.log(error) 
+      })     
+        //{
+        // console.log(response);
        // console.log("this is called");
         //this.loggedIn = true;
-
-      },
-      error: error => console.log(error)
-      
-    })
+     // },    
     // console.log(this.model);
   }
   logout(){
    // this.loggedIn = false;
     this.accountService.logout();         //remove item from local storage
+    this.router.navigateByUrl('/')
   }
  
 }
